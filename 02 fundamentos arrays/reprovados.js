14adb3c2b83c14c041940c52561c9e9a6b931dd9
Pasconsa <saulopascon@hotmail.com>
2022-02-24 17:21:20 -0300

    finalização arrays

---
+++ 02 fundamentos arrays/reprovados.js
@@ -0,0 +1,8 @@
+// Metodo filter  //Retorna o nome de quem foi reprovado
+
+const nomes = ["Ana","Marcos","Maria","Mauro"]
+const notas = [7,4.5,8,7.5]
+
+const reprovados = nomes.filter( (aluno,indice) => notas[indice] < 5)
+
+console.log(`reprovados: ${reprovados}`)
