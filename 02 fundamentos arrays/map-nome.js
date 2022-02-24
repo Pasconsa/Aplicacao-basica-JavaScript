0b32de60787fe10126ccae6524f8bc977046c70a
Pasconsa <saulopascon@hotmail.com>
2022-02-24 17:20:41 -0300

    Finalização aprte arrays

---
+++ 02 fundamentos arrays/map-nome.js
@@ -0,0 +1,8 @@
+// colocar todos os nomes em Maiusculo com Map
+
+let nomes = ["ana Julia","caio Vinicius","Bia silva"]
+
+const nomesAtualizados = nomes.map(nome => nome.toUpperCase())
+
+console.log(nomesAtualizados)
+    
