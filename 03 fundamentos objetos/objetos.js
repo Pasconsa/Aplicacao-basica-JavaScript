03855f2ebb55568094b6e3720a4d5a9da2c33c72
Pasconsa <saulopascon@hotmail.com>
2022-02-25 18:02:49 -0300

    a

---
+++ 03 fundamentos objetos/objetos.js
@@ -0,0 +1,18 @@
+
+
+//Objetos
+    //Arrays organiza informações do mesmo tipo mesma lista
+
+    //Objeto Abstração do mundo real exp: o cliente tem nome , endereço , email
+
+
+const listaCpfs = ["32445566778","2988857766","8393038484"]
+
+//const cliente = ["nome","Andre","idade", 36]
+
+const cliente = {
+    nome:"Saulo",
+    idade:36,
+    cpf:"12134536543",
+    email:"andre@email.com"
+}
