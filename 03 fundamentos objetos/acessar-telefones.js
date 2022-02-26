92bbc12407cf1a91519699437a0950f99af94fa3
Pasconsa <saulopascon@hotmail.com>
2022-02-26 17:15:52 -0300

    tipos de valores

---
+++ 03 fundamentos objetos/acessar-telefones.js
@@ -0,0 +1,11 @@
+//Lista de numeros de pessoas cadastradas n sistema e imprimi las, verificando mais  de um numero no cadastro.
+
+const cliente = {
+    nome:"Saulo",
+    idade:36,
+    cpf:"12134536543",
+    email:"saulo@email.com"
+    fones["5599870707","5521988743124"]
+}
+
+cliente.fones.forEach(fone => console.log(fone))
